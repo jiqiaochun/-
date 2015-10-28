9263aa650dbdc7ab4d87c7e8905f5506eaf5dcc0
jiqiaochun <13011251196@163.com>
2015-10-28 17:21:02 +0800

    Initial Commit

---
+++ ThousandMeters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThousandMeters
+//
+//  Created by 虎动 on 15/10/28.
+//  Copyright © 2015年 虎动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThousandMeters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThousandMeters
+//
+//  Created by 虎动 on 15/10/28.
+//  Copyright © 2015年 虎动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
